483785b96d722e2dd5bf5e0a223466d303747a8b
Brian Shewan <brian.shewan@nscc.ca>
2024-01-11 12:50:32 -0400

    completed stringstream example

--- stringstream/main.cpp
+++ stringstream/main.cpp
@@ -21,21 +21,35 @@ int main() {
 
     if (command.length() > 1) {
         // it must be text
+        std::cout << "You entered some text." << std::endl;
     } else {
         // possible 'I', 'L', 'E', 'D', or 'Q' commands
+        std::cout << "I think you are a command." << std::endl;
 
         // try to extract a number
         ss >> start;
         if (!ss.fail()) {
+            std::cout << "You have a starting number!" << std::endl;
             // success! try to extract out a second number
             ss >> end;
             if (!ss.fail()) {
                 // success! it must be a command!
+                std::cout << "You have an ending number!" << std::endl;
+                std::cout << "I'll assume you are a valid command." << std::endl;
+            } else {
+                std::cout << "You didn't have an ending number." << std::endl;
+                std::cout << "I'll still assume you are a valid command." << std::endl;
+            }
+        } else {
+            // didn't get a number, but could be a command
+            // if the entire input was only one character
+            if (user_input.length() == 1) {
+                std::cout << "I'll assume you are a command!" << std::endl;
+            } else {
+                std::cout << "You must be text instead." << std::endl;
             }
         }
     }
-
-    ss >> command >> start >> end;
 
     std::cout << "Command: " << command << std::endl;
     std::cout << "First: " << start << std::endl;
